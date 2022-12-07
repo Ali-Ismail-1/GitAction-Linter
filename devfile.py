f4f9cb14f7ecffb521d4496df5041d26a5a74de7
Ali-Ismail <ali@cryptofi.tech>
2022-12-07 09:30:19 -0800

    updated devfile to fail lint

---
+++ devfile.py
@@ -0,0 +1,16 @@
+def shouldFailLint1():
+    print("this should fail lint")
+def shouldFailLint2():
+    print("this should fail lint")
+
+
+
+
+
+def shouldFailLint3():
+    print("this should fail lint")
+    
+    
+    
+    
+    
